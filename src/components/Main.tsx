c39a69554550f5834bee97458ab89161ca81c8bb
kzha0676 <zhangkevin7777@gmail.com>
2025-01-20 03:26:26 -0600

    Remove Contact

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,11 +13,11 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/kzha0676" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/kevinzhang001/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Kevin Zhang</h1>
+          <p>Data Analyst</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
