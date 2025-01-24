bc54bd2297633a36cfdfbe929610007aed35ef87
kzha0676 <zhangkevin7777@gmail.com>
2025-01-24 14:43:41 -0600

    Changed expertise section

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,91 +1,44 @@
 import React from "react";
-import '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
-import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
+import Stack from '@mui/material/Stack';
+import { Col } from "react-bootstrap";
+import {
+  SiTableau,
+  SiPython,
+  SiPostgresql 
+} from "react-icons/si";
+import {
+    PiMicrosoftExcelLogo
+  } from "react-icons/pi";
 
-const labelsFirst = [
-    "React",
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
-    "Python",
-    "SQL",
-    "PostgreSQL",
-    "Postman"
-];
-
-const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
-];
-
-const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
-];
-
-function Expertise() {
-    return (
-    <div className="container" id="expertise">
-        <div className="skills-container">
-            <h1>Expertise</h1>
-            <div className="skills-grid">
-                <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsFirst.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
-                </div>
-
-                <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsSecond.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
-                </div>
-
-                <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsThird.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
-                </div>
+const Expertise = () => {
+  return (
+    <div className="skills-container" id= "expertise" >
+            <h1>Skills</h1>
+            <div className="container" style={{ marginTop: '0px' }}>
+                <Stack 
+                    spacing={{ xs: 3, sm: 6 }}
+                    direction="row"
+                    useFlexGap
+                    sx={{ flexWrap: 'wrap', marginTop: '0px' }}
+                    >
+                    <Col xs={6} md={3} className="tech-icons">
+                        <PiMicrosoftExcelLogo />
+                    </Col>
+                    <Col xs={6} md={3} className="tech-icons">
+                        <SiPostgresql/>
+                    </Col>
+                    <Col xs={6} md={3} className="tech-icons">
+                        <SiPython/>
+                    </Col>
+                    <Col xs={6} md={3} className="tech-icons">
+                        <SiTableau />
+                    </Col>
+                </Stack>
             </div>
-        </div>
     </div>
-    );
+  );
 }
+
 
 export default Expertise;
