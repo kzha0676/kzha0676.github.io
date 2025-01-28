855678ac34db45c3d91505b3b515ef2a90b74ef6
kzha0676 <zhangkevin7777@gmail.com>
2025-01-28 10:06:56 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,18 +26,29 @@ function App() {
         window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
       }, []);
 
-    return (
-    <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
-        <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
-        <FadeIn transitionDuration={700}>
-            <Main/>
-            <Expertise/>
-            {/* <Contact/> */}
-            <Timeline/>
-            <Project/>
-        </FadeIn>
-        <Footer />
-    </div>
+      return (
+        <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
+            <div className="layout-wrapper">
+                <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
+                <div className="content-wrapper">
+                    <FadeIn transitionDuration={700}>
+                        <div id="main">
+                            <Main/>
+                        </div>
+                        <div id="expertise">
+                            <Expertise/>
+                        </div>
+                        <div id="history">
+                            <Timeline/>
+                        </div>
+                        <div id="project">
+                            <Project/>
+                        </div>
+                    </FadeIn>
+                    <Footer />
+                </div>
+            </div>
+        </div>
     );
 }
 
